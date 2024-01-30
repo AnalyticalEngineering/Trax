3ad45c78aa0759aacc55de1d974261c5a971cacc
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-01-30 16:36:15 -0600

    Initial Commit

---
+++ Trax/TraxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraxApp.swift
+//  Trax
+//
+//  Created by J. DeWeese on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TraxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
